fc1333f75bb81edbe8d33ade2ca98c1614698443
JCamp-12 <username@users.noreply.github.com>
2018-01-22 22:25:35 -0500

    queue practice, added second way to implement

--- src/queue.js
+++ src/queue.js
@@ -5,14 +5,19 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
+  // Queue is first in, first out
   constructor() {
     this.storage = [];
   }
   enqueue(item) {
     return this.storage.push(item);
+    // unshift and pop together also work
+    // return this.storage.unshift(item);
   }
   dequeue() {
     return this.storage.shift();
+    // unshift and pop together also work
+    // return this.storage.pop();
   }
   get size() {
     return this.storage.length;
